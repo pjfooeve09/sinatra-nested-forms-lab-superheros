26b147ecc87f525e41600aa6c244934ccea48f04
Parham Javadi <pjfooeve09@yahoo.com>
2021-02-26 10:49:09 -0800

    commit tree e1d7ec65b7a4fd8932ed2c3b0cf0b703c0445be9

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,5 +4,19 @@ class App < Sinatra::Base
 
     set :views, Proc.new { File.join(root, "../views/") }
 
+    get '/' do
+      erb :super_hero
+    end
+
+    post '/teams' do
+      @team = Team.new(params[:team])
+      Heroes.new(params[:team][:heroes][0])
+      Heroes.new(params[:team][:heroes][1])
+      Heroes.new(params[:team][:heroes][2])
+      @heroes = Heroes.all
+
+
+      erb :team
+    end
 
 end
---
+++ app/models/hero.rb
@@ -0,0 +1,16 @@
+class Heroes
+    attr_accessor :name, :power, :bio
+    HEROES = []
+  
+    def initialize(params)
+      @name = params[:name]
+      @power = params[:power]
+      @bio = params[:bio]
+      HEROES << self
+    end
+  
+    def self.all
+      HEROES
+    end
+  
+  end
